315f17b1622e42b086707ed201b9dc1ad3a8da67
chirstopher teggatz <categgatzjr@gmail.com>
2024-11-09 17:27:31 -0600

    added application specific applications

--- Intership_Tracker/app/application/[application].tsx
+++ Intership_Tracker/app/application/[application].tsx
@@ -1,27 +1,69 @@
 
 import { useLocalSearchParams, useNavigation } from 'expo-router';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert} from 'react-native';
+import React from 'react';
 import { useEffect } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 
 
 
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function TabTwoScreen() {
-
+  // get page param -> name the page
   const {id} = useLocalSearchParams();
-  const navigation = useNavigation();
 
+  // set the title of the page
+  const navigation = useNavigation();
   useEffect(() => {
     navigation.setOptions({
       title: `Application For "${id}"`, // Set or update the title here
+      
     });
   }, [navigation]);
 
+  // sets on exit and on enter hooks
+  useFocusEffect(
+    React.useCallback(() => {
+      // Code to run when the screen is focused
+      Alert.alert(
+        "Loading Data!", // Title of the alert
+        "This is an alert message", // Message
+      );
+
+      return () => {
+        // Cleanup code when the screen loses focus
+        Alert.alert(
+          "Saving Data!", // Title of the alert
+          "This is an alert message", // Message
+        );
+      };
+    }, [])
+  );
+
+  
+
   return (
     <SafeAreaProvider style = {styles.mainDiv}>
-      <SafeAreaView style = {[styles.bodyDiv, styles.div]}>
-        <Text style = {styles.text}>This application is by {id}</Text>
+      
+      <SafeAreaView style = {[styles.div]}>
+        <Text style = {[styles.text, styles.title]}>General Data</Text>
+        <Text style = {styles.text}>Application for company : "{id}"</Text>
+        <Text style = {styles.text}>Application Status : </Text>
+        <Text style = {styles.text}>(As of)</Text>
+      </SafeAreaView>
+      <SafeAreaView style = {[styles.div]}>
+        <Text style = {[styles.text, styles.title]}>Job Information</Text>
+        <Text style = {styles.text}>Position : [ ]</Text>
+        <Text style = {styles.text}>Recruiter Information: [ ]</Text>
+        <Text style = {styles.text}>Interview Date: [ ]</Text>
+        <Text style = {styles.text}>Interview Location: [ ]</Text>
+        <Text style = {styles.text}>Details</Text>
+        <TextInput style= {{backgroundColor:"white"}}>This is a test</TextInput>
+
+        <Text style = {styles.text}>Link to application Portal</Text>
+        <Text style = {styles.text}>Attach file</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -29,12 +71,14 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   div:{
-    textAlign: "center",
-    alignContent: "center",
-    alignItems: "center",
+    textAlign: "left",
+    alignContent: "flex-start",
+    alignItems: "flex-start",
+    padding: 10,
+    
   },
   mainDiv:{
-
+    
   },
   titleDiv:{
     flex: 1,
